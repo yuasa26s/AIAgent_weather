856d54bdb79420f370a3abbef9481aea837e2731
honoka <uverworldlove@gmail.com>
2026-03-03 22:03:36 +0900

    feat: outfit APIにresponse_modelを追加し動作確認

--- backend/app/api/v1/outfit.py
+++ backend/app/api/v1/outfit.py
@@ -4,13 +4,22 @@ from app.services.outfit_service import generate_outfit
 
 router = APIRouter()
 
+# リクエスト
 class OutfitRequest(BaseModel):
-    uuid: str
-    latitude: float
+    uuid: str # strは文字列
+    latitude: float #小数ありの数字
     longitude: float
     date: str
 
-@router.post("/outfit-recommendation")
+# レスポンス
+class OutfitResponse(BaseModel):
+    temperature: float
+    humidity: int # 整数
+    weather: str
+    wind_speed: float
+    outfit_recommendation: str
+
+@router.post("/outfit-recommendation",
+    response_model=OutfitResponse)
 def outfit_recommendation(data: OutfitRequest):
-    result = generate_outfit(data)
-    return result
+    return generate_outfit(data)
